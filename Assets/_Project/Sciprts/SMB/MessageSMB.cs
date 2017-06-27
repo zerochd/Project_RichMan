a66c32e6f03b856a1634ecae7ae751616f5b6025
unknown <>
2017-06-27 10:17:45 +0800

    add attack anim

---
+++ Assets/_Project/Sciprts/SMB/MessageSMB.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageSMB : StateMachineBehaviour {
+
+	[SerializeField]
+	enum STATE{
+		Enter,
+		Stay,
+		Exit
+	}
+
+	[SerializeField]
+	STATE timing;
+
+	public string functionName;
+
+	public override void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		if(timing == STATE.Enter){
+			animator.SendMessage(functionName,null,SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+	public override void OnStateUpdate (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		if(timing == STATE.Stay){
+			animator.SendMessage(functionName,null,SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+	public override void OnStateExit (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		if(timing == STATE.Exit){
+			animator.SendMessage(functionName,null,SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+}
